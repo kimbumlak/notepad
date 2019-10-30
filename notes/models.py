536c95572f4c61bd3854ea0391db6fdfea965bac
kimbumlak <kimbumlak@gmail.com>
2019-10-30 12:33:43 -0400

    Finished implementing finished_item, recover_item, delete_item

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.shortcuts import redirect, reverse
 
 LABEL_CHOICES = (
     ('P', 'primary'),
@@ -20,3 +20,18 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_finish_item_url(self):
+        return reverse("finish-note-item", kwargs={
+            'id': self.id
+        })
+
+    def get_recover_item_url(self):
+        return reverse("recover-note-item", kwargs={
+            'id': self.id
+        })
+
+    def get_delete_item_url(self):
+        return reverse("delete-note-item", kwargs={
+            'id': self.id
+        })
